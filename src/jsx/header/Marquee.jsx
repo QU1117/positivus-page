16fdd2e4c95b670f77946aa365346b2cb9bc78c6
Dimitry Boyko <n30nv4mp@yandex.ru>
2024-07-27 17:42:54 +0300

    marquee выделен в отдельный компонент JSX

---
+++ src/jsx/header/Marquee.jsx
@@ -0,0 +1,108 @@
+const Marquee = () => {
+  return (
+    <div className="header-companies-marquee">
+      <ul className="header-companies-marquee__content">
+        <li>
+          <img
+            src="../../assets/Amazon.svg"
+            alt="Amazon logo"
+            loading="lazy"
+            decoding="async"
+          />
+        </li>
+        <li>
+          <img
+            src="../../assets/Dribble.svg"
+            alt="Dribble logo"
+            loading="lazy"
+            decoding="async"
+          />
+        </li>
+        <li>
+          <img
+            src="../../assets/Hubspot.svg"
+            alt="Hubspot logo"
+            loading="lazy"
+            decoding="async"
+          />
+        </li>
+        <li>
+          <img
+            src="../../assets/Notion.svg"
+            alt="Notion logo"
+            loading="lazy"
+            decoding="async"
+          />
+        </li>
+        <li>
+          <img
+            src="../../assets/Netflix.svg"
+            alt="Netflix logo"
+            loading="lazy"
+            decoding="async"
+          />
+        </li>
+        <li>
+          <img
+            src="../../assets/Zoom.svg"
+            alt="Zoom logo"
+            loading="lazy"
+            decoding="async"
+          />
+        </li>
+      </ul>
+      <ul className="header-companies-marquee__content" aria-hidden="true">
+        <li>
+          <img
+            src="../../assets/Amazon.svg"
+            alt="Amazon logo"
+            loading="lazy"
+            decoding="async"
+          />
+        </li>
+        <li>
+          <img
+            src="../../assets/Dribble.svg"
+            alt="Dribble logo"
+            loading="lazy"
+            decoding="async"
+          />
+        </li>
+        <li>
+          <img
+            src="../../assets/Hubspot.svg"
+            alt="Hubspot logo"
+            loading="lazy"
+            decoding="async"
+          />
+        </li>
+        <li>
+          <img
+            src="../../assets/Notion.svg"
+            alt="Notion logo"
+            loading="lazy"
+            decoding="async"
+          />
+        </li>
+        <li>
+          <img
+            src="../../assets/Netflix.svg"
+            alt="Netflix logo"
+            loading="lazy"
+            decoding="async"
+          />
+        </li>
+        <li>
+          <img
+            src="../../assets/Zoom.svg"
+            alt="Zoom logo"
+            loading="lazy"
+            decoding="async"
+          />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Marquee;
