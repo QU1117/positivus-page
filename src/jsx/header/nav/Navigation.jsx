b2740fb5821c6530ed4e7887d57a075aeb36c993
Dimitry Boyko <n30nv4mp@yandex.ru>
2024-07-28 20:13:46 +0300

    создан компонент навигации в шапке лэндинга

---
+++ src/jsx/header/nav/Navigation.jsx
@@ -0,0 +1,15 @@
+const Navigation = () => {
+  return (
+    <>
+      <nav className="header-top-nav">
+        <img
+          className="Positivus-logo"
+          src="../../assets/Logo.svg"
+          alt="Positivus logo"
+        />
+      </nav>
+    </>
+  );
+};
+
+export default Navigation;
