268c02b35cc7aa646b324bcbe474f352c517316d
Dimitry Boyko <n30nv4mp@yandex.ru>
2024-07-28 16:54:04 +0300

    добавлен компонент верхней шапки лэндинга

--- src/jsx/header/LandingTopHeader.jsx
+++ src/jsx/header/LandingTopHeader.jsx
@@ -4,8 +4,32 @@ const LandingTopHeader = () => {
   return (
     <>
       <header>
-        <Marquee />
-        <Marquee reverse={true} />
+        <section>
+          <header className="header-title-text">
+            <h1>Navigating the digital landscape for success</h1>
+          </header>
+
+          <img
+            className="header-megaphone-image"
+            src="../../assets/Megaphone.svg"
+            alt="Megaphone"
+          />
+
+          <p className="header-main-text">
+            Our digital marketing agency helps businesses grow and succeed
+            online through a range of services including SEO, PPC, social media
+            marketing, and content creation.
+          </p>
+
+          <div className="button-container">
+            <button type="button" className="primary-button">
+              Book a consultation
+            </button>
+          </div>
+
+          <Marquee />
+          <Marquee reverse={true} />
+        </section>
       </header>
     </>
   );
